9695ef5ce4f1007801f413c196244b832fa74510
Anshorimuslim <ans4175@gmail.com>
2021-10-20 09:48:17 +0700

    fix(home): mutable useRef

--- src/Home.js
+++ src/Home.js
@@ -13,22 +13,37 @@ function Home () {
     () => fetchRandom(),
   )
 
-  const swiped = (direction, id) => {
+  const refSwiped = useRef();
+  refSwiped.current = (direction, id) => {
     const person = characters.find((item) => item.id === id);
     console.log('removing', person)
     setLastDirection(direction)
     setAlreadyRemoved(alreadyRemoved.concat(id))
   }
 
-  const outOfFrame = (id) => {
+  const swiped = (...args) => {
+    refSwiped.current(...args);
+  }
+
+  const refOutOfFrame = useRef();
+  refOutOfFrame.current = (lastDir, id) => {
     const person = characters.find((item) => item.id === id);
     console.log('left the screen', person)
+  }
+
+  const outOfFrame = (...args) => {
+    refOutOfFrame.current(...args);
   }
 
   const undo = () => {
     if (alreadyRemoved.length === 0) return;
     const id = alreadyRemoved[alreadyRemoved.length - 1];
     childRefs.current[id].current.restoreCard();
+  }
+
+  const debug = () => {
+    console.log('alreadyRemoved', alreadyRemoved.length, alreadyRemoved[alreadyRemoved.length-1]);
+    console.log('lastDirection', lastDirection);
   }
 
   for (const character of (characters || [])) {
@@ -63,7 +78,7 @@ function Home () {
                 key={character.id}
                 ref={childRefs.current[character.id]}
                 onSwipe={(dir) => swiped(dir, character.id)}
-                onCardLeftScreen={() => outOfFrame(character.id)}
+                onCardLeftScreen={(dir) => outOfFrame(dir, character.id)}
               >
                 <div style={{ backgroundImage: `url(${character.img})` }} className='card'>
                   <h3>{`like+rt: ${character.likes}`}</h3>
@@ -73,6 +88,7 @@ function Home () {
           </div>
 
           <div className='buttons'>
+            {/*<button onClick={() => debug()}>Debug</button>*/}
             <button onClick={() => undo()}>Undo</button>
           </div>
           <h2 className='infoText'>
