71f879019e03c06456029a8985899c929cd96fd9
Anshorimuslim <ans4175@gmail.com>
2021-10-31 09:31:08 +0700

    fix(App): remove unused BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { QueryClientProvider } from 'react-query';
 import queryClient from './api/queryClient';
 import useGoogleAnalytics from './libs/use-analytics';
