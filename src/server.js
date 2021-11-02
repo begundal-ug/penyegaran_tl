9a3012cbe0e4a90ba5cebc38dde5558bcdf4cafe
Anshorimuslim <ans4175@gmail.com>
2021-11-02 07:13:39 +0700

    fix(server): commented old GA tracker

--- src/server.js
+++ src/server.js
@@ -76,7 +76,7 @@ server
             <div id="root">${markup}</div>
             ${jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
         </body>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <!-- Global site tag (gtag.js) - Google Analytics
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-DECWT3BZWG"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
@@ -84,7 +84,7 @@ server
           gtag('js', new Date());
 
           gtag('config', 'G-DECWT3BZWG');
-        </script>
+        </script -->
       </html>`
       );
     }
