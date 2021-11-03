5e37b92767f9c640d349cd571329be87026a7ccb
Anshorimuslim <ans4175@gmail.com>
2021-11-03 11:22:32 +0700

    feat(component): split Footer component

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => 
+    (<footer>
+        <p>
+            Copyright &copy; Allah SWT dengan segala keindahan ciptaan-Nya.<br />
+            Seluruh kode aplikasi ini tersedia secara secara sumber terbuka <a href="https://github.com/begundal-ug/penyegaran_tl" target="_blank">di Github</a>. 
+            Seluruh data dan gambar diambil dari timeline <a href="https://twitter.com/penyegaran_tl" target="_blank">@penyegaran_tl</a> di Twitter.
+        </p>
+        <p>
+            Sebuah karya kurang kerjaan persembahan dari <a href="https://twitter.com/ans4175" target="_blank">@ans4175</a> dengan <a href="https://twitter.com/anwari888" target="_blank">@anwari888</a>, yang ingin berkarya dan belajar saja. 
+            <strong> Jika Anda ingin foto Anda diambil dari website ini, silakan japri langsung dengan pembuat via Twitter. Kami tidak berafiliasi dengan admin dari @penyegaran_tl.</strong>
+        </p>
+    </footer>)
