96341098858e40161326dcbcd8a880b5bd06c238
Anwari Ilman <anwari.ilman@ctw.bmwgroup.com>
2021-11-04 23:44:20 +0000

    hotfix: typo di tracking ID

--- src/libs/ga-analytics.js
+++ src/libs/ga-analytics.js
@@ -1,11 +1,11 @@
 import ReactGA from "react-ga4"
 
-const TRACKING_ID = "G-DECWT3BZW"
+const TRACKING_ID = "G-DECWT3BZWG"
 
 function init() {
-  // Enable debug mode on the local development environment
+  // Enable testMode on local or development environment
   const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-  ReactGA.initialize(TRACKING_ID, { debug: isDev })
+  ReactGA.initialize(TRACKING_ID, { testMode: isDev })
 }
 
 function sendEvent(payload) {
